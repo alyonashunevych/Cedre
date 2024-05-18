1d929d0023d8d2749fd2ab2f4f7f365fbf56fd6b
Alyona Shunevych <olena17ju06@gmail.com>
2024-05-18 19:28:28 +0300

    small changes

--- src/components/pages/Catalog.js
+++ src/components/pages/Catalog.js
@@ -7,7 +7,7 @@ import { FiltersContext } from "../FiltersContext";
 export default function Catalog() {
   const { filters } = useContext(FiltersContext);
   const priceRanges = ["<$50", "50-100", "100-200", "200-300", ">$300"];
-  window.scrollTo(0, 0);
+ 
   return (
     <div className="catalog">
       <Filter priceRanges={priceRanges} />
